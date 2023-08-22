d7cd30a0d8b8111ddba9a27eefbfdaec18a4fdd1
Stanley643 <nmurigi@kabarak.ac.ke>
2023-08-22 05:27:17 -0700

    main

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_dec(int i);
+int printf_int(int i);
+int print_string(char *str);
+
+#endif
